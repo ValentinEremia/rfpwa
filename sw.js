c34e4665bb59fc7bfaaa97e023ed82eaaf52947f
Valentin.E <98782476+ValentinEremia@users.noreply.github.com>
2022-04-19 13:20:14 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener("install", e =>{
+        e.waitUntil(
+            caches.open("static").then(caches=>{
+                return caches.addAll(["./","./style.css", "./icon-color-192.png"])
+                
+            })
+        )
+
+} );
+
+self.addEventListener("fetch", e =>{
+
+    e.respondWith(
+        caches.match(e.request).then(response =>{
+            return response || fetch(e.request);
+
+        })
+    );
+
+
+   
+});
+
